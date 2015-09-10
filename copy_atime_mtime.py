1c9df5d67e4e2f35ab061593000df8b64c1c791c
John Taylor <john.taylor+github@gmail.com>
2015-09-10 15:32:38 -0400

    initial commit

---
+++ copy_atime_mtime.py
@@ -0,0 +1,14 @@
+
+import os,sys
+
+# copies the src atime/mtime (access/modifiy) attribs to dst
+# ctime can not be changed
+
+src = sys.argv[1]
+dst = sys.argv[2]
+
+meta = os.stat(src)
+os.utime(dst, (meta.st_atime,meta.st_mtime))
+
+print( os.stat(src) )
+print( os.stat(dst) )
