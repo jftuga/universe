8229f4d34e7f2ded372e0c2778456b18707c2654
John Taylor <john.taylor+github@gmail.com>
2015-09-11 01:11:40 -0400

    added comment about utf-16

--- is_binary--python3.py
+++ is_binary--python3.py
@@ -5,6 +5,7 @@
 
 from itertools import zip_longest
 
+# include 0 for UTF-16 unicode text strings
 textchars = bytearray([0,7,8,9,10,12,13,27]) + bytearray(range(0x20, 0x100))
 textdict = dict(zip_longest(textchars,[''],fillvalue=''))
 is_binary_string = lambda data: True if not len(data) else bool(data.translate(textdict))
