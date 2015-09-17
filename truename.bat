5170ad020d66dba08cc8ea40e8349a80b1ec1e4f
John Taylor <john.taylor+github@gmail.com>
2015-09-17 10:26:19 -0400

    added support for unc paths

--- truename.bat
+++ truename.bat
@@ -1,2 +1,4 @@
 @echo off
-for /f "usebackq tokens=1,2,3" %%i in (`net use^|findstr /i %~d1`) do @echo "%%k%~p1%~n1%~x1"
+
+rem use %%k for normal shares and %%j for unc shares
+for /f "usebackq tokens=1,2,3" %%i in (`net use^|findstr /i %~d1`) do @echo "%%j%~p1%~n1%~x1"
