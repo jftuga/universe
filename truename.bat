77e0033e9017e0467d8882ef1d2d0a3b90da5901
John Taylor <john.taylor+github@gmail.com>
2015-09-17 10:28:14 -0400

    added support for DFS share names

--- truename.bat
+++ truename.bat
@@ -1,4 +1,4 @@
 @echo off
 
-rem use %%k for normal shares and %%j for unc shares
+rem use %%k for normal share names and %%j for DFS share names such as \\my.domain\apps
 for /f "usebackq tokens=1,2,3" %%i in (`net use^|findstr /i %~d1`) do @echo "%%j%~p1%~n1%~x1"
