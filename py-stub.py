686819805c3282546e72882f8d9582e2f64a3978
John Taylor <johntaylor@gmail.com>
2015-12-15 15:18:56 -0500

    py-stub.py:: initial commit

---
+++ py-stub.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# pystub.py
+# -John Taylor
+# MMM-DD-YYYY
+
+# python code template
+
+import sys
+
+#############################################################################
+
+def main():
+	
+	return 0
+
+#############################################################################
+
+if "__main__" == __name__:
+	sys.exit( main() )
