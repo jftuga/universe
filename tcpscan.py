7c90dfce0bb13e026e6f7beecef9a827806b63ab
John Taylor <johntaylor@gmail.com>
2016-02-12 20:09:12 -0500

    tcpscan.py:: added doc strings and more error checking

--- tcpscan.py
+++ tcpscan.py
@@ -5,7 +5,10 @@ tcpscan.py
 -John Taylor
 
 A simple, multi-threaded TCP port scanner
+Python 3.5
 """
+
+# https://stackoverflow.com/questions/3898572/what-is-the-standard-python-docstring-format
 
 import sys
 import socket
@@ -16,8 +19,8 @@ from datetime import datetime
 from ipaddress import ip_network
 from random import shuffle
 
-pgm_version = "1.08"
-pgm_date = "Jan-15-2016 06:09"
+pgm_version = "1.09"
+pgm_date = "Feb-12-2016 20:09"
 
 # default maximum number of concurrent threads, changed with -T
 max_workers = 50
@@ -26,9 +29,9 @@ max_workers = 50
 connect_timeout = 0.07
 
 # list of ports to scan if -p is not given on the command line
-default_port_list = "20,21,22,23,25,47,53,69,80,110,113,123,135,137,138,139,143,161,179,194,201,311,389,427,443,445,465,500,513,514,515,530,548,554,563,587,593,601,631,636,660,674,691,694,749,751,843,873,901,902,903,987,990,992,993,994,995,1000,1167,1234,1433,1434,1521,1528,1723,1812,1813,2000,2049,2375,2376,2077,2078,2082,2083,2086,2087,2095,2096,2222,2433,2483,2484,2638,3000,3260,3283,3306,3389,3478,3690,4000,5000,5432,5433,5800,5801,5900,5901,6000,6667,7000,8000,8080,8443,8880,8888,9000,9001,9418,9998,27017,27018,27019,28017,32400"
+default_port_list = "20,21,22,23,25,47,53,69,80,110,113,123,135,137,138,139,143,161,179,194,201,311,389,427,443,445,465,500,513,514,515,530,548,554,563,587,593,601,631,636,660,674,691,694,749,751,843,873,901,902,903,987,990,992,993,994,995,1000,1167,1234,1433,1434,1521,1528,1723,1812,1813,2000,2049,2375,2376,2077,2078,2082,2083,2086,2087,2095,2096,2222,2433,2483,2484,2638,3000,3260,3283,3306,3389,3478,3690,4000,5000,5432,5433,6000,6667,7000,8000,8080,8443,8880,8888,9000,9001,9418,9998,27017,27018,27019,28017,32400"
 
-# initialize other globals...
+# initialize variables
 active_hosts = defaultdict(list)
 hosts_scanned = 0
 skipped_hosts = 0
@@ -39,8 +42,27 @@ skipped_port_list = []
 
 #############################################################################################
 
-def scan_one_host(ip,ports):
+def scan_one_host(ip: str, ports: str) -> None:
+	"""Scan a host for the given open ports.
+	
+	Args:
+		ip: An IPv4 address in dotted-quad notation.
+
+		ports: A list of ports in either range format (x-y) or
+		    list format (a,b,c,d).
+
+	Returns:
+		Does not return any values.
+
+	Raises:
+		Does not raise any errors.
+	"""
+
 	global args, max_workers, connect_timeout, hosts_scanned
+
+	if ports.find("-") > -1 and ports.find(",") > -1:
+		print("\nError: For -p option, port list cannot contain both a port range and list of ports\n")
+		sys.exit(1)
 
 	hosts_scanned += 1
 	if ports.find("-") > 0 and ports.find(",") == -1:
@@ -49,8 +71,11 @@ def scan_one_host(ip,ports):
 		start = int(start)
 		end = int(end)
 		if end < start:
-			print("\nError: For -p option, ending port is less that starting port\n")
-			sys.exit(1)		
+			print("\nError: For -p option, ending port is less than starting port\n")
+			sys.exit(1)
+		if end > 65535:
+			print("\nError: For -p option, ending port is greater than 65535\n")
+			sys.exit(1)
 		port_list = list(range(start,end+1))
 	else:
 		# comma separated list of ports, can also include a single port
@@ -64,10 +89,28 @@ def scan_one_host(ip,ports):
 
 #############################################################################################
 
-def scan_one_port(ip,port):
+def scan_one_port(ip: str, port:str) -> bool:
+	"""Scan the given host for one open port.
+	
+	Args:
+		ip: An IPv4 address in dotted-quad notation.
+
+		port: A TCP port number 1-65535 (as a string).
+
+	Returns:
+		Returns True if the port is open and
+		False otherwise.
+
+	Raises:
+		Does not raise any errors.
+	"""
+
 	global args, fp_output, active_hosts, opened_ports, ports_scanned
 	global max_workers, connect_timeout, skipped_port_list, skipped_ports
 	port = int(port)
+	if port > 65535:
+		print("\nError: Port is greater than 65535\n")
+		return False
 
 	if port in skipped_port_list:
 		if args.verbose:
@@ -110,7 +153,21 @@ def scan_one_port(ip,port):
 
 #############################################################################################
 
-def create_skipped_port_list(ports):
+def create_skipped_port_list(ports:str) -> None:
+	"""Create a Python list from the given argument string.
+	
+	Args:
+		ports: A list of ports in either range format (x-y) or
+		    list format (a,b,c,d).
+
+	Returns:
+		Modifies the global skipped_port_list, which is a Python
+		list to include all ports that will be excluded from scanning.
+
+	Raises:
+		Does not raise any errors.
+	"""
+
 	global skipped_port_list 
 
 	if ports.find("-") > 0 and ports.find(",") == -1:
@@ -119,7 +176,7 @@ def create_skipped_port_list(ports):
 		start = int(start)
 		end = int(end)
 		if end < start:
-			print("\nError: For -X option, ending port is less that starting port\n")
+			print("\nError: For -X option, ending port is less than starting port\n")
 			sys.exit(1)
 		skipped_port_list = list(range(start,end+1))
 	else:
@@ -128,7 +185,19 @@ def create_skipped_port_list(ports):
 
 #############################################################################################
 
-def main():
+def main() -> None:
+	"""Process command-line arguments, scan hosts/ports, print results.
+	
+	Args:
+		None
+
+	Returns:
+		None
+
+	Raises:
+		Does not raise any errors.
+	"""
+
 	global args, fp_output, default_port_list
 	global max_workers, connect_timeout
 	global skipped_hosts, skipped_ports, hosts_scanned
