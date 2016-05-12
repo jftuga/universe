82a996867d07713ee463bae66b18c7a26ee7b76d
John Taylor <johntaylor@gmail.com>
2016-05-11 21:39:26 -0400

    safe_print.py:: added optional isError argument

--- safe_print.py
+++ safe_print.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 # safe_print.py
 # -John Taylor
@@ -7,8 +8,12 @@
 
 import sys
 
-def safe_print(data):
+def safe_print(data,isError=False):
+    dest = sys.stdout if not isError else sys.stderr
     # can also use 'replace' instead of 'ignore' for errors= parameter
-    print( str(data).encode(sys.stdout.encoding, errors='ignore').decode(sys.stdout.encoding) )
+    print( str(data).encode(sys.stdout.encoding, errors='ignore').decode(sys.stdout.encoding), file=dest )
 
+if "__main__" == __name__:
+    safe_print("normal message")
+    safe_print("error_mesage", True)
 
