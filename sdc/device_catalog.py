4cf0a7549192a2c1a47e9227b0e4f4c1b9487ecd
John Taylor <johntaylor@gmail.com>
2017-07-02 10:40:46 -0400

    sdc.py

---
+++ sdc/device_catalog.py
@@ -0,0 +1,13 @@
+
+# SAVE:: command output is save to a file
+device_commands = {}
+device_commands["hp_procurve"]    = ( "no page", "SAVE::show run" )
+device_commands["hp_comware"]     = ( "SAVE::display current-configuration", )
+device_commands["paloalto_panos"] = ( "set cli pager off", "SAVE::show config running")
+device_commands["ubiquiti_edge"]  = ( "configure", "run terminal length 9999", "SAVE::run show configuration all", "exit")
+
+re_device_excludes = {}
+re_device_excludes["hp_procurve"] = "^$", "Running config", "^;"
+
+re_device_includes = {}
+re_device_includes["hp_procurve"] = "[0-9a-f]{6}-[0-9a-f]{6}", "Up","Down"
