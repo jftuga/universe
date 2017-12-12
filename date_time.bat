46c47443d24e19eef7753fdcecac23dedc919972
John Taylor <johntaylor@gmail.com>
2017-12-12 10:01:22 -0500

    date_time.bat:: added zero padding when hour is less than 10

--- date_time.bat
+++ date_time.bat
@@ -13,6 +13,6 @@ for /f "usebackq" %%g in (`gdate +"%%Y%%m%%d_%%H%%M%%S"`) do set GDATE=%%g
 @echo batch : %NOW%
 @echo gdate : %GDATE%
 
-rem one liner :-)
-for /f "usebackq tokens=1,2,3,4,5,6,7 delims=/:. " %%a in (`echo %DATE% %TIME%`) do set NOW=%%d%%b%%c_%%e%%f%%g
+rem Use zero-padding when hour is than 10...
+for /f "usebackq tokens=1,2,3,4,5,6,7 delims=/:. " %%a in (`echo %DATE% %TIME%`) do if %%e lss 10 (set NOW=%%d%%b%%c_0%%e%%f%%g) else (set NOW=%%d%%b%%c_%%e%%f%%g)
 @echo now   : %NOW%
