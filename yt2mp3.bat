6f0174cf42296f4b9b2783964478d31b4e2b7506
John Taylor <johntaylor@gmail.com>
2018-01-13 15:33:17 -0500

    now handle multiple URLs on cmd line

--- yt2mp3.bat
+++ yt2mp3.bat
@@ -1,20 +1,16 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-rem converts a YouTube video to local MP3 file
+:LOOP
+if "%~1" neq "" (
+	@echo.
+	@echo =====================================================================
+	@echo %1 
+	@echo =====================================================================
+	@echo.
+	youtube-dl.exe -k -x --audio-format mp3 --audio-quality 2 %1
+	shift
+	goto :LOOP
+)
 
-set URL=%1
-if not defined URL goto ERROR
 
-:RUN
-h:\bin\youtube-dl.exe -t --extract-audio --audio-format mp3 -k "%URL%"
-goto END
-
-:ERROR
-@echo.
-@echo Give YouTube URL on command line, within double-quotes
-@echo.
-goto END
-
-:END
-endlocal
