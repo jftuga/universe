8cbf6bc537879e45987b0c1be8b41050e2efe8dd
John Taylor <johntaylor@gmail.com>
2018-02-01 06:26:14 -0500

    now using StringComparer.Ordinal and TryGetValue

--- freq.cs
+++ freq.cs
@@ -1,11 +1,4 @@
-﻿
-// display the number of occurences of the same lines in a file
-// or read from STDIN
-
-// 'tbl' key=the line in the file; value=number of occurences
-
-
-using System;
+﻿using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Collections.Concurrent;
@@ -17,16 +10,15 @@ namespace freq
     {
         static void Main(string[] args)
         {
-            Dictionary<string, int> tbl = new Dictionary<string, int>();
-            // this did not seem any faster...
-            //ConcurrentDictionary<string, int> tbl = new ConcurrentDictionary<string, int>();
+            Dictionary<string, int> tbl = new Dictionary<string, int>(StringComparer.Ordinal);
+            int value;
 
             if (args.Length == 0) // read from STDIN
             {
                 string line;
                 while ((line = Console.ReadLine()) != null)
                 {
-                    tbl[line] = (tbl.ContainsKey(line)) ? tbl[line] += 1 : tbl[line] = 1;
+                    tbl[line] = (tbl.TryGetValue(line, out value)) ? tbl[line] = value + 1 : tbl[line] = 1;
                 }
             } else // read from file given on cmd line
             {
@@ -40,13 +32,13 @@ namespace freq
                 var data = File.ReadLines(fname);
                 foreach (string line in data)
                 {
-                    tbl[line] = (tbl.ContainsKey(line)) ? tbl[line] += 1 : tbl[line] = 1;
-                    // this did not seem any faster (used when using ConcurrentDictionary)
-                    //tbl.AddOrUpdate(line, 1, (key, oldValue) => oldValue + 1);
+                    tbl[line] = (tbl.TryGetValue(line,out value)) ? tbl[line] = value+1 : tbl[line] = 1;
                 }
             }
 
+            // https://stackoverflow.com/questions/21411384/sort-dictionary-string-int-by-value
             var tbl_sorted = tbl.OrderByDescending(pair => pair.Value).ThenBy(pair => pair.Key);
+
             foreach (KeyValuePair<string, int> entry in tbl_sorted)
             {
                 Console.WriteLine("{0,7}\t{1}", entry.Value, entry.Key);
