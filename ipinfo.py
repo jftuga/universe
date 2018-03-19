13fe44ccb8c496396f6ec943bdc1d60bca005802
John Taylor <johntaylor@gmail.com>
2018-03-19 08:51:40 -0400

    convert tabs to spaces

--- ipinfo.py
+++ ipinfo.py
@@ -17,81 +17,82 @@ import urllib.request
 ##########################################################################
 
 def resolve(obj):
-	n = -1
-	try:
-		n = socket.gethostbyname_ex(obj)
-	except:
-		print()
-		print("Could not resolve: %s" % (obj))
-		print()
-		sys.exit(1)
+    n = -1
+    try:
+        n = socket.gethostbyname_ex(obj)
+    except:
+        print()
+        print("Could not resolve: %s" % (obj))
+        print()
+        sys.exit(1)
 
-	if type(n) is str:
-		return list( [2] )
-	else:
-		return n[2]
+    if type(n) is str:
+        return list( [2] )
+    else:
+        return n[2]
 
 ##########################################################################
 
 def get_ip_info(ip:str):
-	url = "https://ipinfo.io/%s/json" % (ip)
-	text = ""
-	try:
-		response = urllib.request.urlopen(url)
-		data = response.read()
-		text = data.decode('utf-8')
-	except:
-		print()
-		print("Could not connect to: %s" % (url))
-		print()
-		sys.exit(1)
-	return json.loads(text)
+    url = "https://ipinfo.io/%s/json" % (ip)
+    text = ""
+    try:
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        text = data.decode('utf-8')
+    except:
+        print()
+        print("Could not connect to: %s" % (url))
+        print()
+        sys.exit(1)
+    return json.loads(text)
 
 ##########################################################################
 
 def print_result(obj:str):
-	result = []
-	all_keys = ( "input", "ip", "hostname", "org", "loc", "city", "region", "country" )
-	for key in all_keys:
-		if key in obj:
-			result.append("'%s':'%s'" % (key, obj[key]))
+    result = []
+    all_keys = ( "input", "ip", "hostname", "org", "loc", "city", "region", "country" )
+    for key in all_keys:
+        if key in obj:
+            result.append("'%s':'%s'" % (key, obj[key]))
 
-	print("{ %s }" % (", ".join(result)))
+    print("{ %s }" % (", ".join(result)))
 
 
 ##########################################################################
 
 def main():
-	if len(sys.argv) < 2:
-		print("Usage:")
-		print("%s: [ IP address | host name | URL ] ..." % (sys.argv[0]))
-		print()
-		return
+    if len(sys.argv) < 2:
+        print("Usage:")
+        print("%s: [ IP address | host name | URL ] ..." % (sys.argv[0]))
+        print()
+        return
 
-	ip_re = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+    ip_re = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
 
-	for obj in sys.argv[1:]:
-		if obj.find("://") > 1:
-			host_name = obj.split("/")[2]
-			host_ip_list = resolve( host_name )
-		else:
-			match = ip_re.findall(obj)
+    for obj in sys.argv[1:]:
+        if obj.find("://") > 1:
+            host_name = obj.split("/")[2]
+            host_ip_list = resolve( host_name )
+            obj = host_name
+        else:
+            match = ip_re.findall(obj)
 
-			if len(match):
-				host_ip_list = match
-			else:
-				host_ip_list = resolve( obj )
+            if len(match):
+                host_ip_list = match
+            else:
+                host_ip_list = resolve( obj )
 
-		for ip in host_ip_list:
-			json_result = get_ip_info(ip)
-			json_result["input"] = obj
-			print_result(json_result)
+        for ip in host_ip_list:
+            json_result = get_ip_info(ip)
+            json_result["input"] = obj
+            print_result(json_result)
 
-			if(len(host_ip_list) > 1):
-				time.sleep(0.25)
+            if(len(host_ip_list) > 1):
+                time.sleep(0.25)
 
 ##########################################################################
 
 if __name__ == '__main__':
-	main()
+    main()
 
