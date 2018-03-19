7ce1926fccf72e61238021793d9c9c76d5f4d0c7
John Taylor <johntaylor@gmail.com>
2018-03-19 10:28:39 -0400

    added distance calculation

--- ipinfo.py
+++ ipinfo.py
@@ -6,6 +6,9 @@ ipinfo.py
 Query https://ipinfo.io for IP address info including geographic location when given IP address, host name or URL
 Multiple arguments can be given on cmd line
 
+Example:
+ipinfo.py uga.edu gatech.edu clemson.edu sc.edu utk.edu auburn.edu unc.edu www.uky.edu ufl.edu olemiss.edu www.virginia.edu louisiana.edu umiami.edu missouri.edu utexas.edu texastech.edu
+
 MIT License; Copyright (c) 2018 John Taylor
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -13,6 +16,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 """
 
+import geopy.distance
 import json
 import re
 import socket
@@ -40,7 +44,7 @@ def resolve(obj):
 
 ##########################################################################
 
-def get_ip_info(ip:str):
+def get_ip_info(ip:str) -> str:
     url = "https://ipinfo.io/%s/json" % (ip)
     text = ""
     try:
@@ -56,7 +60,13 @@ def get_ip_info(ip:str):
 
 ##########################################################################
 
-def build_row(obj:str) -> tuple:
+def str2tup(loc:str) -> tuple:
+    a,b = loc.split(",")
+    return ( float(a), float(b) )
+
+##########################################################################
+
+def build_row(obj:str, my_loc:str) -> tuple:
     result = []
     all_keys = ( "input", "ip", "hostname", "org", "loc", "city", "region", "country" )
     for key in all_keys:
@@ -65,12 +75,39 @@ def build_row(obj:str) -> tuple:
         else:
             result.append("")
 
+    if "loc" in all_keys:
+        try:
+            distance = "%.2f" % (geopy.distance.vincenty(str2tup(my_loc), str2tup(obj["loc"])).mi)
+            result.append(distance)
+        except:
+            result.append("")
+    else:
+        result.append("")
+
     return tuple(result)
 
 ##########################################################################
 
 def build_tbl(tbl:VeryPrettyTablePatched, obj:str):
     tbl.add_row(obj)
+
+##########################################################################
+
+def get_my_coords() -> str:
+    url = "https://ipinfo.io/json"
+    text = ""
+    try:
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        text = data.decode('utf-8')
+    except:
+        print()
+        print("Could not connect to: %s" % (url))
+        print()
+        sys.exit(1)
+    
+    obj = json.loads(text)
+    return obj["loc"]
 
 ##########################################################################
 
@@ -82,9 +119,10 @@ def main():
         return
 
     ip_re = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+    my_loc = get_my_coords()
 
     tbl = VeryPrettyTablePatched()
-    tbl.field_names = ( "input", "ip", "hostname", "org", "loc", "city", "region", "country" )
+    tbl.field_names = ( "input", "ip", "hostname", "org", "loc", "city", "region", "country", "distance (mi)" )
     tbl.align = "l"
 
     for obj in sys.argv[1:]:
@@ -103,7 +141,7 @@ def main():
         for ip in host_ip_list:
             json_result = get_ip_info(ip)
             json_result["input"] = obj
-            tbl.add_row(build_row(json_result))
+            tbl.add_row(build_row(json_result,my_loc))
 
             if(len(host_ip_list) > 1):
                 time.sleep(0.25)
