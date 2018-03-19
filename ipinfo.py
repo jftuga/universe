b90d6ca511cb531bcd24b6faf591ade511792e0b
John Taylor <johntaylor@gmail.com>
2018-03-19 09:12:52 -0400

    Converted output to use VeryPrettyTablePatched

--- ipinfo.py
+++ ipinfo.py
@@ -5,6 +5,12 @@ ipinfo.py
 
 Query https://ipinfo.io for IP address info including geographic location when given IP address, host name or URL
 Multiple arguments can be given on cmd line
+
+MIT License; Copyright (c) 2018 John Taylor
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 """
 
 import json
@@ -13,6 +19,7 @@ import socket
 import sys
 import time
 import urllib.request
+from veryprettytablepatched import VeryPrettyTablePatched
 
 ##########################################################################
 
@@ -49,15 +56,21 @@ def get_ip_info(ip:str):
 
 ##########################################################################
 
-def print_result(obj:str):
+def build_row(obj:str) -> tuple:
     result = []
     all_keys = ( "input", "ip", "hostname", "org", "loc", "city", "region", "country" )
     for key in all_keys:
         if key in obj:
-            result.append("'%s':'%s'" % (key, obj[key]))
+            result.append(obj[key])
+        else:
+            result.append("")
 
-    print("{ %s }" % (", ".join(result)))
+    return tuple(result)
 
+##########################################################################
+
+def build_tbl(tbl:VeryPrettyTablePatched, obj:str):
+    tbl.add_row(obj)
 
 ##########################################################################
 
@@ -69,6 +82,10 @@ def main():
         return
 
     ip_re = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+
+    tbl = VeryPrettyTablePatched()
+    tbl.field_names = ( "input", "ip", "hostname", "org", "loc", "city", "region", "country" )
+    tbl.align = "l"
 
     for obj in sys.argv[1:]:
         if obj.find("://") > 1:
@@ -86,10 +103,12 @@ def main():
         for ip in host_ip_list:
             json_result = get_ip_info(ip)
             json_result["input"] = obj
-            print_result(json_result)
+            tbl.add_row(build_row(json_result))
 
             if(len(host_ip_list) > 1):
                 time.sleep(0.25)
+
+    print( tbl.get_string())
 
 ##########################################################################
 
