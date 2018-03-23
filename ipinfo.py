93853e77f0654f4839c4c0382a3e678819b85471
John Taylor <johntaylor@gmail.com>
2018-03-23 16:06:32 -0400

    web requests are now multithreaded and run in parallel

--- ipinfo.py
+++ ipinfo.py
@@ -16,6 +16,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 """
 
+import concurrent.futures
 import geopy.distance
 import json
 import re
@@ -23,7 +24,10 @@ import socket
 import sys
 import time
 import urllib.request
+from collections import defaultdict
 from veryprettytablepatched import VeryPrettyTablePatched
+
+pgm_version = "2.0"
 
 ##########################################################################
 
@@ -37,26 +41,31 @@ def resolve(obj):
         print()
         sys.exit(1)
 
+    return (obj, n[2])
+
     if type(n) is str:
-        return list( [2] )
+        return list( n[2] )
     else:
         return n[2]
 
 ##########################################################################
 
-def get_ip_info(ip:str) -> str:
+def get_ip_info(ip:str, hostname:str) -> str:
     url = "https://ipinfo.io/%s/json" % (ip)
     text = ""
     try:
         response = urllib.request.urlopen(url)
         data = response.read()
         text = data.decode('utf-8')
+        
     except:
         print()
         print("Could not connect to: %s" % (url))
         print()
         sys.exit(1)
-    return json.loads(text)
+    info = json.loads(text)
+    info["input"] = hostname
+    return info
 
 ##########################################################################
 
@@ -88,11 +97,6 @@ def build_row(obj:str, my_loc:str) -> tuple:
 
 ##########################################################################
 
-def build_tbl(tbl:VeryPrettyTablePatched, obj:str):
-    tbl.add_row(obj)
-
-##########################################################################
-
 def get_my_coords() -> str:
     url = "https://ipinfo.io/json"
     text = ""
@@ -117,6 +121,9 @@ def main():
         print("%s: [ IP address | host name | URL ] ..." % (sys.argv[0]))
         print()
         return
+    
+    # max number of concurrent web requests
+    max_workers = 30
 
     ip_re = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
     my_loc = get_my_coords()
@@ -125,31 +132,53 @@ def main():
     tbl.field_names = ( "input", "ip", "hostname", "org", "loc", "city", "region", "country", "distance (mi)" )
     tbl.align = "l"
 
+    host_ip_list = []
+    host_ip_tbl = defaultdict(list)
+
     for obj in sys.argv[1:]:
         if obj.find("://") > 1:
             host_name = obj.split("/")[2]
-            host_ip_list = resolve( host_name )
+            host_ip_list.append( host_name )
             obj = host_name
         else:
             match = ip_re.findall(obj)
 
             if len(match):
-                host_ip_list = match
+                host_ip_list.append(match[0])
             else:
-                host_ip_list = resolve( obj )
+                host_ip_list.append(obj)
 
-        for ip in host_ip_list:
-            json_result = get_ip_info(ip)
-            json_result["input"] = obj
-            tbl.add_row(build_row(json_result,my_loc))
+    # host_ip_list is a list containing either a host name with the URL stripped or an IP address (1 per cmd-line argument)
+    #print( host_ip_list )
 
-            if(len(host_ip_list) > 1):
-                time.sleep(0.25)
+    with concurrent.futures.ThreadPoolExecutor(max_workers,thread_name_prefix="resolve") as executor:
+        dns_result = {executor.submit(resolve,host): host for host in host_ip_list}
+        for future in concurrent.futures.as_completed(dns_result):
+            if future.done():
+                fu_res = future.result()
+                for ip in fu_res[1]:
+                    host_ip_tbl[ip] = fu_res[0]
+
+    # host_ip_tbl is a dict with key=ip, value=hostname or IP address
+    # the same value can be used by multiple distinct keys
+    #for ip in host_ip_tbl:
+    #    print(ip, host_ip_tbl[h])
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers,thread_name_prefix="get_ip_info") as executor:
+        json_result = {executor.submit(get_ip_info,ip,host_ip_tbl[ip]): ip for ip in host_ip_tbl}
+        for future in concurrent.futures.as_completed(json_result):
+            if future.done():
+                ip_table = future.result()
+                tbl.add_row(build_row(ip_table,my_loc))
 
     print( tbl.get_string())
 
 ##########################################################################
 
 if __name__ == '__main__':
+    time_start = time.time()
     main()
+    time_end = time.time()
+    print()
+    print("elapsed time: %.2f seconds" % (time_end - time_start) )
 
