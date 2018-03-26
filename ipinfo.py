52cdc19c2ca858b822740112e807afb1a66983d6
John Taylor <johntaylor@gmail.com>
2018-03-26 09:34:59 -0400

    replace geopy.distance dependency with vincenty()

--- ipinfo.py
+++ ipinfo.py
@@ -7,7 +7,7 @@ Query https://ipinfo.io for IP address info including geographic location when g
 Multiple arguments can be given on cmd line
 
 Example:
-ipinfo.py uga.edu gatech.edu clemson.edu sc.edu utk.edu auburn.edu unc.edu www.uky.edu ufl.edu olemiss.edu www.virginia.edu louisiana.edu umiami.edu missouri.edu utexas.edu texastech.edu
+ipinfo uga.edu gatech.edu clemson.edu sc.edu utk.edu auburn.edu unc.edu www.uky.edu ufl.edu olemiss.edu www.virginia.edu louisiana.edu umiami.edu missouri.edu utexas.edu texastech.edu
 
 MIT License; Copyright (c) 2018 John Taylor
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -17,8 +17,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 """
 
 import concurrent.futures
-import geopy.distance
 import json
+import math
 import re
 import socket
 import sys
@@ -27,7 +27,74 @@ import urllib.request
 from collections import defaultdict
 from veryprettytablepatched import VeryPrettyTablePatched
 
-pgm_version = "2.1"
+pgm_version = "2.2"
+
+# constants for vincenty()
+# WGS 84
+a = 6378137  # meters
+f = 1 / 298.257223563
+b = 6356752.314245  # meters; b = (1 - f)a
+MILES_PER_KILOMETER = 0.621371
+MAX_ITERATIONS = 200
+CONVERGENCE_THRESHOLD = 1e-12  # .000,000,000,001
+
+##########################################################################
+
+# adapted from: https://pypi.python.org/pypi/vincenty/
+def vincenty(point1, point2, miles=True):
+
+    # short-circuit coincident points
+    if point1[0] == point2[0] and point1[1] == point2[1]:
+        return 0.0
+
+    U1 = math.atan((1 - f) * math.tan(math.radians(point1[0])))
+    U2 = math.atan((1 - f) * math.tan(math.radians(point2[0])))
+    L = math.radians(point2[1] - point1[1])
+    Lambda = L
+
+    sinU1 = math.sin(U1)
+    cosU1 = math.cos(U1)
+    sinU2 = math.sin(U2)
+    cosU2 = math.cos(U2)
+
+    for iteration in range(MAX_ITERATIONS):
+        sinLambda = math.sin(Lambda)
+        cosLambda = math.cos(Lambda)
+        sinSigma = math.sqrt((cosU2 * sinLambda) ** 2 +
+                             (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda) ** 2)
+        if sinSigma == 0:
+            return 0.0  # coincident points
+        cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * cosLambda
+        sigma = math.atan2(sinSigma, cosSigma)
+        sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma
+        cosSqAlpha = 1 - sinAlpha ** 2
+        try:
+            cos2SigmaM = cosSigma - 2 * sinU1 * sinU2 / cosSqAlpha
+        except ZeroDivisionError:
+            cos2SigmaM = 0
+        C = f / 16 * cosSqAlpha * (4 + f * (4 - 3 * cosSqAlpha))
+        LambdaPrev = Lambda
+        Lambda = L + (1 - C) * f * sinAlpha * (sigma + C * sinSigma *
+                                               (cos2SigmaM + C * cosSigma *
+                                                (-1 + 2 * cos2SigmaM ** 2)))
+        if abs(Lambda - LambdaPrev) < CONVERGENCE_THRESHOLD:
+            break  # successful convergence
+    else:
+        return None  # failure to converge
+
+    uSq = cosSqAlpha * (a ** 2 - b ** 2) / (b ** 2)
+    A = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq)))
+    B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq)))
+    deltaSigma = B * sinSigma * (cos2SigmaM + B / 4 * (cosSigma *
+                 (-1 + 2 * cos2SigmaM ** 2) - B / 6 * cos2SigmaM *
+                 (-3 + 4 * sinSigma ** 2) * (-3 + 4 * cos2SigmaM ** 2)))
+    s = b * A * (sigma - deltaSigma)
+
+    s /= 1000  # meters to kilometers
+    if miles:
+        s *= MILES_PER_KILOMETER  # kilometers to miles
+
+    return round(s, 6)
 
 ##########################################################################
 
@@ -81,7 +148,7 @@ def build_row(obj:str, my_loc:str) -> tuple:
 
     if "loc" in all_keys:
         try:
-            distance = "%.2f" % (geopy.distance.vincenty(str2tup(my_loc), str2tup(obj["loc"])).mi)
+            distance = "%.2f" % (vincenty(str2tup(my_loc), str2tup(obj["loc"]),miles=True))
             result.append(distance)
         except:
             result.append("")
