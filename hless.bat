a7ef3c225e5b2267008ca2a8e72771c3880e1aa4
John Taylor <johntaylor@gmail.com>
2018-05-09 09:39:50 -0400

    hless.bat:: initial commit

---
+++ hless.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+rem possible formatters: [default=TerminalFormatter]
+rem img.BmpImageFormatter img.GifImageFormatter html.HtmlFormatter img.PngImageFormatter img.JpgImageFormatter
+rem rtf.RtfFormatter terminal256.Terminal256Formatter terminal.TerminalFormatter terminal256.TerminalTrueColorFormatter
+rem
+rem possible styles:
+rem default emacs friendly colorful autumn murphy manni monokai perldoc pastie borland trac native fruity bw
+rem vim vs tango rrt xcode igor paraiso-light paraiso-dark lovelace algol algol_nu arduino rainbow_dash abap
+
+set OUTPUT=%TEMP%\hless--%RANDOM%.%RANDOM%.htm
+pygmentize -f html -O full,style=default,linenos=1 -g -o %OUTPUT% %1
+if exist %OUTPUT% (
+    %OUTPUT% && del %OUTPUT%
+)
+
