3d264179ebad63d802d0e042f7a579de0c51d0d8
John Taylor <johntaylor@gmail.com>
2018-05-24 19:33:26 -0400

    sleepuntil.py: added error checking

--- sleepuntil.py
+++ sleepuntil.py
@@ -5,13 +5,31 @@ import datetime
 
 #################################################################
 
+def usage():
+    print()
+    print("Usage: %s HR:MM:SS:MS" % (sys.argv[0]))
+    print("hour should be given in 24-hr time")
+    print("seconds and microseconds are optional")
+    print()
+
+#################################################################
+
 def main():
+    if 1 == len(sys.argv):
+        usage()
+        return 1
+
     slots = sys.argv[1].split(":")
 
-    hour = int(slots[0])
-    minute = int(slots[1])
-    second = 0 if 2 == len(slots) else int(slots[2])
-    microsecond = 0 if 3 == len(slots) else int(slots[3])
+    try:
+        hour = int(slots[0])
+        minute = int(slots[1])
+        second = 0 if 2 == len(slots) else int(slots[2])
+        microsecond = 0 if len(slots) <= 3 else int(slots[3])
+    except:
+        usage()
+        return 1
+
 
     start = datetime.datetime.today()
     until = start.replace(hour=hour, minute=minute, second=second, microsecond=microsecond)
@@ -35,8 +53,11 @@ def main():
     except KeyboardInterrupt:
         print("You pressed Ctrl+C")
         print("final time  :", current)
+        return 1
+
+    return 0
 
 #################################################################
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
