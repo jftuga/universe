00ffc767fed96dff7d5a8e5dcca49c2a26525486
John Taylor <johntaylor@gmail.com>
2020-02-02 15:52:36 -0500

    margs:: small optimization

--- margs.bat
+++ margs.bat
@@ -7,4 +7,4 @@ rem example: margs.bat code *.yaml
 rem result:  code r:\code\a.yaml r:\code\test\b.yaml
 rem pipe this result to 'clip' and then right click to paste into cmd prompt
 
-dir %2 /s/b | mawk "{ a =sprintf('%%s %%s',a,$0)} END { b=substr(a,2); printf('%%s %%s\n',p,b) }" p="%1"
+dir %2 /s/b | mawk "{ a =sprintf('%%s %%s',a,$0)} END { printf('%%s %%s\n',p,substr(a,2)) }" p="%1"
