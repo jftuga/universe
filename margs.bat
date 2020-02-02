ce30484633ab697900351b1d446918b4b040d612
John Taylor <johntaylor@gmail.com>
2020-02-02 15:57:15 -0500

    margs.bat:: remove unneeded double-quotes, add 2nd example

--- margs.bat
+++ margs.bat
@@ -3,8 +3,11 @@ setlocal
 
 rem arg1: cmd
 rem arg2: file glob
-rem example: margs.bat code *.yaml
+rem example1 : margs.bat code *.yaml
 rem result:  code r:\code\a.yaml r:\code\test\b.yaml
 rem pipe this result to 'clip' and then right click to paste into cmd prompt
+rem or pipe straight to cmd
+rem
+rem example 2: margs.bat "zip -9r test.zip" *.bat | cmd
 
-dir %2 /s/b | mawk "{ a =sprintf('%%s %%s',a,$0)} END { printf('%%s %%s\n',p,substr(a,2)) }" p="%1"
+dir %2 /s/b | mawk "{ a =sprintf('%%s %%s',a,$0)} END { printf('%%s %%s\n',p,substr(a,2)) }" p=%1
