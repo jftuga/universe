ff665b674216a6c84e208d1558f01cff4ee2f5dc
John Taylor <johntaylor@gmail.com>
2020-05-22 08:12:25 -0400

    ensure results are displayed by path order

--- pywhich.py
+++ pywhich.py
@@ -10,10 +10,11 @@ Iterate through the PATH environment variable to search for an executable given 
 
 import os
 import os.path
+import platform
 import sys
 import concurrent.futures
 
-VERSION="1.2.1"
+VERSION="1.2.2"
 
 # common Windows executable file extensions
 all_ext = ( "bat", "cmd", "com", "cpl", "exe", "inf", "ini", "job", "lnk", "msc", "msi", "msp", "mst", 
@@ -29,7 +30,18 @@ def usage():
     print("Example: pywhich curl")
     print()
 
-def search(dname:str, pgm:str):
+def search(order:int, dname:str, pgm:str) -> list:
+    """search for all programs starting with pgm, 
+       for 'less', this could return 'less.exe', 'less.ps1', 'lessecho.exe', 'lesskey.exe'
+
+       Args:
+        order: the order in the path in which a program appears
+
+        dname: the directory name to search for
+
+        pgm: the program name to search for
+    """
+    results = []
     if not os.path.exists(dname):
         #print("Error: path not found:", dname)
         return
@@ -41,7 +53,8 @@ def search(dname:str, pgm:str):
         if f.lower() == pgm:
             found = os.path.join(dname,f)
             match = found
-            print(found)
+            #print(found)
+            results.append((order,found))
         orig = f
         f_ext = os.path.splitext(f)[1]
         f_ext = f_ext[1:]
@@ -50,7 +63,9 @@ def search(dname:str, pgm:str):
             if f_ext in all_ext:
                 found = os.path.join(dname,orig)
                 if match != found:
-                    print(found)
+                    #print(found)
+                    results.append((order,found))
+    return results
 
 
 def main():
@@ -60,12 +75,30 @@ def main():
     pgm = sys.argv[1]
     pgm = pgm.lower()
     all_paths = os.environ['PATH']
-    all_paths = ".;" + all_paths
+    if "Windows" == platform.system():
+        all_paths = ".;" + all_paths
     path_list = all_paths.split(";")
-    with concurrent.futures.ThreadPoolExecutor(len(path_list)) as executor:
-        {executor.submit(search, path, pgm): path for path in path_list}
+    
+    i = 0
+    ordered = {}
+    for p in path_list:
+        ordered[p] = i
+        i+=1
 
+    ordered_results = []
+    with concurrent.futures.ThreadPoolExecutor(len(path_list)) as executor:
+        result = {executor.submit(search, ordered[path], path, pgm): path for path in path_list}
+        for future in concurrent.futures.as_completed(result):
+            if future.done():
+                all_pgms = future.result()
+                if len(all_pgms):
+                    for pgm in all_pgms:
+                        ordered_results.append(pgm)
+
+    # ensure results are displayed by path order
+    ordered_results.sort(key = lambda x: x[0])
+    for pgm in ordered_results:
+        print(pgm[1])
 
 if "__main__" == __name__:
     main()
- 
